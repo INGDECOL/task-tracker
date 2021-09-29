8a874a4eda7a2c4f796cb0a3410d4b35cf207997
INGDECOL <tousmane89@gmail.com>
2021-09-29 12:04:38 +0000

    Filter NavBar & showing filtered projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,28 @@
 <template>
-  <div v-if="projects.length">
-    <div v-for="project in projects" :key="project.id">
-      <SingleProject :project = "project" @delete="handleDelete" @complete="handleComplete"/>
+<div class="home">
+    <FilterNav @filterChange=" current = $event" :current="current"/>
+    <div v-if="projects.length">
+      <div v-for="project in filteredProjects" :key="project.id">
+        <SingleProject :project = "project" @delete="handleDelete" @complete="handleComplete"/>
+      </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
 export default {
   name: 'Home',
   components:{
-    SingleProject
+    SingleProject,
+    FilterNav
   },
   data () {
     return {
       projects : [],
+      current: 'all'
     }
   },
   methods:{
@@ -30,6 +36,9 @@ export default {
         return project.id === id
       })
       p.complete = !p.complete
+    },
+    filter(event){
+      console.log(event)
     }
   },
   mounted () {
@@ -38,5 +47,16 @@ export default {
       .then(data => this.projects = data)
       .catch(err =>console.log(err.message))
   },
+  computed:{
+    filteredProjects(){
+      if(this.current =='completed'){
+        return this.projects.filter(project =>  project.complete)
+      }
+      if(this.current == 'ongoing'){
+        return this.projects.filter(project =>  !project.complete)
+      }
+      return this.projects
+    }
+  }
 }
 </script>
